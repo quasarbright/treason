7eca68e1c1d133c801f3a223b6d930dd5c769792
Mike Delmonaco <mthundershock@gmail.com>
2026-01-09 11:20:12 -0500

    edits with michael

--- server.rkt
+++ server.rkt
@@ -37,8 +37,6 @@
 ;; A Response
 ;; A Notification
 
-;; TODO headers in ascii body in utf-8?
-
 (define (main [in (current-input-port)] [out (current-output-port)])
   (let loop ()
     (define msg (read-message in))
@@ -46,69 +44,98 @@
       [(request id method parameters)
        (write-message (response id "foo" 'null) out)]
       [(notification method parameters)
-       (void)])))
+       (void)])
+    (loop)))
 
 ;; -> message?
 (define (read-message [in (current-input-port)])
-  (read-line in 'return-linefeed)
-  (read-line in 'return-linefeed)
-  (define js (read-json in))
-  (cond
-    [(and (hash-has-key? js 'id)
-          (hash-has-key? js 'method)
-          (hash-has-key? js 'params))
-     (request (hash-ref js 'id)
-              (string->symbol (hash-ref js 'method))
-              (hash-ref js 'params))]
-    [(and (hash-has-key? js 'method)
-          (hash-has-key? js 'params))
-     (notification (string->symbol (hash-ref js 'method))
-                   (hash-ref js 'params))]
-    [(and (hash-has-key? js 'id)
-          (or (hash-has-key? js 'result)
-              (hash-has-key? js 'error)))
-     (response (hash-ref js 'id)
-               (hash-ref js 'result 'null)
-               (hash-ref js 'error 'null))]))
+  (define headers (read-headers in))
+  (define content-length (string->number (hash-ref headers 'Content-Length (lambda () (error 'read-message "missing Content-Length header")))))
+  (define bytes (read-bytes content-length in))
+  (define js-str (bytes->string/utf-8 bytes))
+
+  (define js (string->jsexpr js-str))
+  (match js
+    [(hash* ['id id] ['method method] ['params params])
+     (request id
+              (string->symbol method)
+              params)]
+    [(hash* ['method method] ['params params])
+     (notification (string->symbol method)
+                   params)]
+    [(hash* ['id id] ['result result])
+     (response id result 'null)]
+    [(hash* ['id id] ['error error])
+     (response id 'null error)]))
 
 (module+ test
+  (define-check (check-read-of-write msg)
+    (let ([msg-v msg])
+      (check-equal? (read-message (open-input-string (with-output-to-string (lambda () (write-message msg-v)))))
+                    msg-v)))
   ;; read-message tests
-  (check-equal? (read-message (open-input-string "blah\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 1, \"method\": \"foo\", \"params\": []}"))
-                (request 1 'foo '()))
-  
-  (check-equal? (read-message (open-input-string "blah\r\n\r\n{\"jsonrpc\": \"2.0\", \"method\": \"bar\", \"params\": {}}"))
-                (notification 'bar '#hasheq()))
-  
-  (check-equal? (read-message (open-input-string "blah\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"result\": \"success\"}"))
-                (response 2 "success" 'null))
+  (check-read-of-write (request 1 'foo '()))
+  (check-read-of-write (notification 'bar '#hasheq()))
+  (check-read-of-write (response 2 "success" 'null))
+  (check-read-of-write (response 2 'null "failure"))
 
-  (check-equal? (read-message (open-input-string "blah\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}"))
-                (response 2 'null "failure")))
+  ;; multiple header lines
+  (check-equal? (read-message (open-input-string "Content-Length: 58\r\nContent-Type: application/vscode-jsonrpc; charset=utf-8\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}"))
+                (response 2 'null "failure"))
+  ;; 2 reads
+  (let ([in (open-input-string "Content-Length: 47\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}Content-Length: 47\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}")])
+    (check-equal? (read-message in)
+                  (response 2 'null "failure"))
+    (check-equal? (read-message in)
+                  (response 2 'null "failure")))
+  ;; 2 reads with newline in between. works bc content length
+  (let ([in (open-input-string "Content-Length: 48\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}\nContent-Length: 47\r\n\r\n{\"jsonrpc\": \"2.0\", \"id\": 2, \"error\": \"failure\"}")])
+    (check-equal? (read-message in)
+                  (response 2 'null "failure"))
+    (check-equal? (read-message in)
+                  (response 2 'null "failure"))))
+
+;; -> (Hash Symbol String)
+(define (read-headers [in (current-input-port)])
+  (let loop ([headers (hash)])
+    (define line (read-line in 'return-linefeed))
+    (define result (regexp-match #px"([^:]+): (.*)" line))
+    (match result
+      [(list _ name value)
+       (loop (hash-set headers (string->symbol name)
+                               value))]
+      [#f headers])))
+
+(module+ test
+  (check-equal? (read-headers (open-input-string "Content-Length: 2\r\nContent-Type: application/vscode-jsonrpc; charset=utf-8\r\n\r\n{}"))
+                (hash 'Content-Length "2" 'Content-Type "application/vscode-jsonrpc; charset=utf-8")))
 
 ;; message? -> void?
 (define (write-message msg [out (current-output-port)])
-    (define js
-      (match msg
-        [(request id method params)
-         (hash 'jsonrpc "2.0"
-               'id id
-               'method (symbol->string method)
-               'params params)]
-        [(response id result error)
-         (if (eq? error 'null)
-             (hash 'jsonrpc "2.0"
-                   'id id
-                   'result result)
-             (hash 'jsonrpc "2.0"
-                   'id id
-                   'error error))]
-        [(notification method params)
-         (hash 'jsonrpc "2.0"
-               'method (symbol->string method)
-               'params params)]))
-    (define str (jsexpr->string js))
-    (define len (string-length str))
-    (display (format "Content-Length: ~a\r\n\r\n~a" len str)))
+  (define js
+    (match msg
+      [(request id method params)
+       (hash 'jsonrpc "2.0"
+             'id id
+             'method (symbol->string method)
+             'params params)]
+      [(response id result error)
+       (if (eq? error 'null)
+           (hash 'jsonrpc "2.0"
+                 'id id
+                 'result result)
+           (hash 'jsonrpc "2.0"
+                 'id id
+                 'error error))]
+      [(notification method params)
+       (hash 'jsonrpc "2.0"
+             'method (symbol->string method)
+             'params params)]))
+  (define str (jsexpr->string js))
+  (define bytes (string->bytes/utf-8 str))
+  (define len (bytes-length bytes))
+  (display (format "Content-Length: ~a\r\n\r\n~a" len str))
+  (flush-output out))
 
 (module+ test
   (check-equal? (with-output-to-string
@@ -124,4 +151,8 @@
                 "Content-Length: 43\r\n\r\n{\"id\":2,\"jsonrpc\":\"2.0\",\"result\":\"success\"}")
   (check-equal? (with-output-to-string
                   (lambda () (write-message (response 2 'null "failure"))))
-                "Content-Length: 42\r\n\r\n{\"error\":\"failure\",\"id\":2,\"jsonrpc\":\"2.0\"}"))
+                "Content-Length: 42\r\n\r\n{\"error\":\"failure\",\"id\":2,\"jsonrpc\":\"2.0\"}")
+  ;; bytes content length
+  (check-equal? (with-output-to-string
+                  (lambda () (write-message (response 2 'null "😎"))))
+                "Content-Length: 39\r\n\r\n{\"error\":\"😎\",\"id\":2,\"jsonrpc\":\"2.0\"}"))
